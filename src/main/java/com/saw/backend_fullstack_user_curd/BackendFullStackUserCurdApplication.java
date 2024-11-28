9b95046ba12d23bd0b64776ab20a74bce0918756
saw <saw@saws-MacBook-Pro.local>
2024-11-28 20:27:47 +0700

    init project

---
+++ src/main/java/com/saw/backend_fullstack_user_curd/BackendFullStackUserCurdApplication.java
@@ -0,0 +1,13 @@
+package com.saw.backend_fullstack_user_curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFullStackUserCurdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFullStackUserCurdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saw/backend_fullstack_user_curd/BackendFullStackUserCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saw.backend_fullstack_user_curd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFullStackUserCurdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
